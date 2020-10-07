803dc6cb59cafe9da0e0adba255c8f6480acbb47
Alex Villecco <alexlecco24@gmail.com>
2020-10-07 20:11:38 -0300

    setup request.js and axios.js

--- src/request.js
+++ src/request.js
@@ -1,5 +1,14 @@
-const APIKEY = "";
+const API_KEY = "3090277f5365ca2cf8c7266a4ff18c4f";
 
-const requests = {};
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+};
 
 export default requests;
