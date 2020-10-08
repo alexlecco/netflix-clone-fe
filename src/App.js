32c25efc5f3b3108f494bf3837fd536deef9531a
Alex Villecco <alexlecco24@gmail.com>
2020-10-07 23:18:09 -0300

    create Row and render title prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey Clever Programmer! Let's build a Netflix clone today!</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function Row({ title }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+}
+
+export default Row;
