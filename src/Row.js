97a14fbd429bde3a79835e61cbfbbb1045b7eb07
Alex Villecco <alexlecco24@gmail.com>
2020-10-08 09:48:55 -0300

    map movies to show movies poster in Row

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
@@ -16,6 +18,16 @@ function Row({ title, fetchUrl }) {
   return (
     <div>
       <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
